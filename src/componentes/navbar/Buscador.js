0fc6adb879bbe05df7862ea4e4b88efac79627b1
David Garnica <garnicadavid1@gmail.com>
2022-11-23 21:12:19 -0300

    crear navbar

---
+++ src/componentes/navbar/Buscador.js
@@ -0,0 +1,9 @@
+import './Buscador.css'
+
+export function Buscador(){
+    return <div class="buscador">
+            <input type="text" id="buscador" placeholder="Buscar..." />
+            <button type="button" id="buscar">Buscar!</button>
+        </div>
+}
+
---
+++ src/componentes/navbar/Nav.js
@@ -0,0 +1,8 @@
+import { Buscador } from "./Buscador";
+import './Nav.css';
+
+export function Nav(){
+    return  <nav id="navegador">
+                <Buscador/>
+            </nav>
+}

89203e4816855d886e785d1401895906ee2fcf7d
JuanMorales <juanmoralesp19@gmail.com>
2022-11-24 10:46:27 -0300

    Enrutamiento (Comienzo)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import PaginaNoEncontrada from "./pages/PaginaNoEncontrada";
+import Principal from './pages/Principal';
 
 export function App() {
   return (
     <BrowserRouter>
+    <NavBar />
+      <Routes>
+        <Route path="/" element={ <Principal /> } />
+        <Route path="/registro" element={<div>About</div>} />
+        <Route path='*' element={ <PaginaNoEncontrada /> } />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/pages/Principal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Principal = () => {
+  return <div>Principal</div>;
+};
+
+export default Principal;

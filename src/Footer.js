0a9b54474e4d028ac01d2f06b35841f15fc905c2
RLauraS <R_LauraS@yahoo.com.ar>
2022-11-25 20:08:04 -0300

    agregando footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { EnlacesFooter } from './components/footer/footer'
 
 const Footer = () => {
   return (
     <div>
-        
+        <EnlacesFooter/>
     </div>
   )
 }

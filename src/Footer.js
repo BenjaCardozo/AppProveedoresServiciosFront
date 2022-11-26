d068f2234a1643d1c158f655dd39ac92ab53fa0d
BenjaCardozo <benjamin.cardozo0@gmail.com>
2022-11-26 04:37:38 -0300

    haciendo formulario

--- src/Footer.js
+++ src/Footer.js
@@ -2,19 +2,27 @@ import React from "react";
 import { Redes } from "./components/footer/Redes";
 
 const Footer = () => {
+
+  const year = new Date().getFullYear();
+  const companyName = "Tu Profesional";
+  const sobreNos = "La aplicación perfecta para buscar al profesional que necesites de la manera más rápida y eficaz."
   return (
     <div>
-      <div class="container">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div class="col-md-4 d-flex align-items-center">
-            <a
+      <div className="container">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          {/* <div className="col-md-4 d-flex align-items-center"> */}
+            {/* <a
               href="/"
-              class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-            ></a>
-            <span class="mb-3 mb-md-0 text-muted">
-              &copy; 2022 TuProfesional
-            </span>
-          </div>
+              className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+            ></a> */}
+            <p className="col-md-4 mb-0 text-muted">
+              &copy; {year} {companyName}
+            </p>
+            <br/>
+            <p className="text-muted col-md-5">
+              {sobreNos}
+            </p>
+          {/* </div> */}
             <Redes />
         </footer>
       </div>
--- src/components/registro/Seleccionar.js
+++ src/components/registro/Seleccionar.js
@@ -1,10 +1,40 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Seleccionar.css";
 
 function Seleccionar() {
+  const [rol, setRol] = useState("");
   return (
     <div>
-        
+      <div className="row align-items-md-stretch">
+        <div className="col-md-4" onClick={ r=>{setRol("proveedor");}}>
+          <Link to={`/registro/${rol}`} >
+            <button className="h-100 p-5 text-bg-dark rounded-3" type="button">
+              <h2>Quiero ofrecer mis servicios!</h2>
+              <p>
+                Registrate para que los clientes puedan contactarte de una
+                manera más efectiva y segura.
+              </p>
+            </button>
+          </Link>
+        </div>
+        <div className="col-md-4" onClick={ r=>{setRol("cliente");}}>
+        <Link to={`/registro/${rol}`}>
+            <button
+              className="h-100 p-5 bg-light border rounded-3"
+              type="button"
+            >
+              <h2>Quiero contratar un profesional!</h2>
+              <p>
+                Registrate para encontrarte con el profesional que necesitabas
+                en tu barrio.
+              </p>
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Seleccionar
+export default Seleccionar;

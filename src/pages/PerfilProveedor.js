bf760c6a1f7e4fe9804a15b38b2e8bbe37669e54
David Garnica <garnicadavid1@gmail.com>
2022-11-29 19:11:40 -0300

    Crear perfil proveedor

---
+++ src/pages/PerfilProveedor.js
@@ -0,0 +1,9 @@
+import Nav from "../components/navbar/Nav";
+import { ProveedorVista } from "../components/perfil/ProveedorVista";
+
+export function PerfilProveedor(){
+    return <>
+        <Nav/>
+        <ProveedorVista/>
+    </>
+}
